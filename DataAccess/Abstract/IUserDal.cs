4696bdf4e33e894356e22113e66db05ed728a4d2
Furkan Aydemir <furkanaydemir12@hotmail.com>
2025-08-05 21:48:15 +0300

    Start Crud Operations

---
+++ DataAccess/Abstract/IUserDal.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IUserDal
+    {
+        void Add(User user);
+
+        void Update(User user);
+
+
+        void Delete(User user);
+
+        List<User> GetAll();// ->dönüştipi List olmalı
+        User Get(int id);
+        //Get işlenmi bittiğinde user classı vermek zorundayız->dönüştipi
+
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,59 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Context;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfUserDal : IUserDal
+    {
+        
+
+        public void Add(User user)
+        {
+            using (var context = new SimpleContextDb())
+            {
+                context.Users.Add(user);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(User user)
+        {
+            using (var context = new SimpleContextDb())
+            {
+                context.Users.Remove(user);
+                context.SaveChanges();
+            }
+        }
+
+        public User Get(int id)
+        {
+            using (var context = new SimpleContextDb())
+            {
+                return context.Users.Where(X => X.Id == id).FirstOrDefault();
+            }
+        }     
+
+        public List<User> GetAll()
+        {
+            using (var context = new SimpleContextDb())
+            {
+                return context.Users.ToList();
+            }
+        }
+
+        public void Update(User user)
+        {
+            using (var context = new SimpleContextDb())
+            {
+                context.Users.Update(user);
+                context.SaveChanges();
+            }
+        }
+    }
+}
